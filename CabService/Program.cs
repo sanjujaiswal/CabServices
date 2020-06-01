6238c5f1e4d62f9dec37bd50f3445a41ed6f04be
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-06-01 17:33:29 +0530

    ADDED: Welcome to cab services

---
+++ CabService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CabService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator Services!!");
+        }
+    }
+}
