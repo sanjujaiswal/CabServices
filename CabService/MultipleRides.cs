6bb58400a349ee5542bb389221ae2a5919664420
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-06-01 19:59:37 +0530

    ADDED: Calculate and show invoice for multiple rides

---
+++ CabService/MultipleRides.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabService
+{
+    public class MultipleRides
+    {
+        readonly public double distance;
+        readonly public double time;
+        public MultipleRides(double rideDistance, double rideTime)
+        {
+            this.distance = rideDistance;
+            this.time = rideTime;
+        }
+    }
+}
